cf470d536327419074d31c928a24ec06a0cbff37
alexey.rotar <ar_mail@inbox.ru>
2023-07-10 23:47:58 +0500

    0.3

--- notes.py
+++ notes.py
@@ -1,4 +1,6 @@
-import datetime
+﻿import datetime
+from datetime import datetime
+from datetime import timedelta
 
 def read_file(filename):
     with open(filename, 'r') as data:
@@ -21,9 +23,9 @@ def add_note(filename, head = '', body = '', date = ''):
         if max_id < int(notes_array[i][0]): 
             max_id = int(notes_array[i][0])
     next_id = max_id + 1
-    head = input('Заголовок: ')
-    body = input('Текст заметки: ') 
-    current_date = datetime.datetime.now()
+    head = input('Введите заголовок: ')
+    body = input('Введите текст заметки: ')
+    current_date = datetime.now()
     date = current_date.strftime('%d/%m/%y %H:%M:%S')
     new_item = []
     new_item.append(str(next_id))
@@ -32,15 +34,35 @@ def add_note(filename, head = '', body = '', date = ''):
     new_item.append(date)
     notes_array.append(new_item)
     write_file(filename, notes_array)
+    print('Новая заметка добавлена успешно.\n')
 
 def show_all_notes(filename):
-    notes_array = read_file(filename)    
-    for i in range(1,len(notes_array)):
-        print("ID: ", notes_array[i][0], "Заголовок: ", notes_array[i][1],"Текст заметки: ", notes_array[i][2], "Дата: ", notes_array[i][3])
-
-def show_item(filename, ID):
     notes_array = read_file(filename)
-    print(notes_array[ID])
+    print(f"\n{'ID':<7}{'Заголовок':<30}{'Текст заметки':<80}{'Дата создания (изменения)'}")
+    for i in range(1,len(notes_array)):
+        print(f"{notes_array[i][0]:<7}{notes_array[i][1]:<30}{notes_array[i][2]:<80}{notes_array[i][3]}")
+    print()
+
+def show_notes_by_date(filename, date):
+    flag = True
+    notes_array = read_file(filename)
+    check_date = datetime.strptime(date, '%d/%m/%y')
+    for i in range(1,len(notes_array)):
+        note_date = datetime.strptime(notes_array[i][3], '%d/%m/%y %H:%M:%S')
+        if (check_date <= note_date) and (check_date + timedelta(hours=24) > note_date):
+            if flag == True:
+                print(f"\n{'ID':<7}{'Заголовок':<30}{'Текст заметки':<80}{'Дата создания (изменения)'}")
+                flag = False
+            print(f"{notes_array[i][0]:<7}{notes_array[i][1]:<30}{notes_array[i][2]:<80}{notes_array[i][3]}")
+    if flag == True:
+        print('Нет заметок на указанную дату!\n')
+    print()
+
+def show_note(filename, index):
+    notes_array = read_file(filename)
+    print(f"\n{'ID':<7}{'Заголовок':<30}{'Текст заметки':<80}{'Дата создания (изменения)'}")
+    print(f"{notes_array[index][0]:<7}{notes_array[index][1]:<30}{notes_array[index][2]:<80}{notes_array[index][3]}")
+    print()
 
 def find_note_ID(filename, ID):
     notes_array = read_file(filename)
@@ -51,17 +73,16 @@ def find_note_ID(filename, ID):
 
 def change_note(filename, item_ID):
     notes_array = read_file(filename)
-    head = input('Заголовок: ')
-    body = input('Текст заметки: ')
-    current_date = datetime.datetime.now()
+    head = input('Введите заголовок: ')
+    body = input('Введите текст заметки: ')
+    current_date = datetime.now()
     date = current_date.strftime('%d/%m/%y %H:%M:%S')
     notes_array[item_ID][1] = head
     notes_array[item_ID][2] = body
     notes_array[item_ID][3] = date
-
     write_file(filename, notes_array)
-    print('Измененная запись: ')
-    print(notes_array[item_ID])
+    print('Изменения внесены успешно.')
+    show_note(filename, item_ID)
 
 def delete_note(filename, ID):
     notes_array = read_file(filename)
@@ -69,34 +90,45 @@ def delete_note(filename, ID):
     write_file(filename, notes_array)
 
 def menu():
-    print('1 - Показать все записи')
-    print('2 - Добавить запись')
-    print('3 - Изменить запись')
-    print('4 - Удалить запись')
-    user_operation = int(input())
-
-    if user_operation == 1:
-        show_all_notes(filename)
-    elif user_operation == 2: 
-        add_note(filename)
-    elif user_operation == 3:
-        user_operation = int(input('Укажите ID записи для внесения изменений: '))
-        ID = find_note_ID(filename, user_operation)
-        if ID != None and ID != 0:
-            print('Будут вноситься изменения в запись: ')
-            show_item(filename, ID)
-            change_note(filename, ID)
-        else:
-            print('Запись с таким ID не найдена!')
-    elif user_operation == 4:
-        user_operation = int(input('Укажите ID записи для удаления: '))
-        ID = find_note_ID(filename, user_operation)
-        if ID != None and ID != 0:
-            print('Удалена запись: ')
-            show_item(filename, ID)
-            delete_note(filename, ID)
-        else:
-            print('Запись с таким ID не найдена!')
+    user_input = None
+    while user_input != 0:
+        print('1 - Показать все заметки')
+        print('2 - Найти заметки по дате')
+        print('3 - Добавить заметку')
+        print('4 - Изменить заметку')
+        print('5 - Удалить заметку')
+        print('0 - Выход')
+        user_input = int(input('Выберите пункт меню: '))
+        match user_input:
+            case 1:
+                show_all_notes(filename)
+            case 2:
+                date_input = input('Укажите дату в формате ДД/ММ/ГГ: ')
+                show_notes_by_date(filename, date_input)
+            case 3:
+                add_note(filename)
+            case 4:
+                date_input = int(input('Укажите ID заметки для внесения изменений: '))
+                ID = find_note_ID(filename, date_input)
+                if ID != None and ID != 0:
+                    print('Редактирование заметки: ')
+                    show_note(filename, ID)
+                    change_note(filename, ID)
+                else:
+                    print('Заметка с таким ID не найдена!\n')
+            case 5:
+                date_input = int(input('Укажите ID зметки для удаления: '))
+                ID = find_note_ID(filename, date_input)
+                if ID != None and ID != 0:
+                    print('Удалена заметка: ')
+                    show_note(filename, ID)
+                    delete_note(filename, ID)
+                else:
+                    print('Заметка с таким ID не найдена!\n')
+            case 0:
+                print('Работа завершена.\n')
+            case _:
+                print('Введено неверное значение!\n')
 
 filename = 'notes.csv'
 menu()
